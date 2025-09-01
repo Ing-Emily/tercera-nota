ed020982f79a270f0b63c21b8f2a5b3cb7fa1c8c
Emily <castillaemili3@gmail.com>
2025-09-01 07:44:32 -0500

    se arregla estructura

--- src/unidad1/Intento.java
+++ src/unidad1/Intento.java
@@ -1,8 +1,6 @@
-public class Tomas {
-
+package unidad1;
+public class Intento {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!");
-        System.out.println("¡tengo mucha hambre!");
     }
-
 }
--- src/unidad1/Tomas.java
+++ src/unidad1/Tomas.java
@@ -1,5 +1,9 @@
-public class Intento {
+package unidad1;
+public class Tomas {
+
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!");
+        System.out.println("¡tengo mucha hambre!");
     }
+
 }
